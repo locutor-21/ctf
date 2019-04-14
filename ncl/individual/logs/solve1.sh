7f284c5d43450332b1d1bde3e9dd925971c5f792
Pedro <pdcmpa@gmail.com>
2019-04-14 18:43:02 -0400

    Renamed ctf/ncl/single to ctf/ncl/individual and added some files.

---
+++ ncl/individual/logs/solve1.sh
@@ -0,0 +1,1 @@
+grep -c /robots.txt IIS.log
--- ncl/single/crypto/dec4.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python 
-
-enc = "3-33-555-33-8-33 999-666-88-777 22-2-7777-44 44-444-7777-8-666-777-999"
-
-dec = "delete your bash history"
-
-print dec
